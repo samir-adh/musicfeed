f3db244611153fd16c9c1301a73d183dd3b6bff1
Samir Abdallah <abdousamir8@gmail.com>
2026-01-13 18:51:18 +0100

    api now returns artist data and releases

--- musicfeed_api/services/DeezerApiService.py
+++ musicfeed_api/services/DeezerApiService.py
@@ -1,19 +1,31 @@
 from typing import Optional
 import requests
 from dataclasses import dataclass
+from enum import Enum
+
 
 @dataclass
 class RecordData():
-    record_id : int
-    title : str
-    release_date : str
+    record_id: int
+    title: str
+    release_date: str
+    record_type: str
 
-@dataclass
+
 class ArtistData():
-    deezer_id: int
-    name: str
-    records = list[RecordData]
 
+    def __init__(self, deezer_id: int, name: str) -> None:
+        self.deezer_id: int = deezer_id
+        self.name: str = name
+        self.records: list[RecordData] = []
+
+    def addRecord(self, record: RecordData):
+        self.records.append(record)
+
+
+class APIDataNotFoundError(Exception):
+    """Raised when expected data is missing from API response"""
+    pass
 
 
 class DeezerApiService():
@@ -32,11 +44,25 @@ class DeezerApiService():
                 return None
             deezer_id = response.json()["id"]
             name = response.json()["name"]
-            records_list_json = self.session.get(
-                self.BASE_URL + f""
-            )
 
-
-            return ArtistData(deezer_id, name)
+            artist = ArtistData(deezer_id, name)
+            self.resolve_albums_list(artist)
+            return artist
         except Exception:
             return None
+
+    def resolve_albums_list(self, artist: ArtistData):
+        response = self.session.get(
+            self.BASE_URL + f"/artist/{artist.deezer_id}/albums/"
+        )
+        if "error" in response.json() and response.json()["error"]["type"] == "DataException":
+            raise APIDataNotFoundError
+        albums_list = response.json()["data"]
+        for record_data in albums_list:
+            record = RecordData(
+                record_data["id"],
+                record_data["title"],
+                record_data["release_date"],
+                record_data["record_type"]
+            )
+            artist.addRecord(record)
--- musicfeed_api/views.py
+++ musicfeed_api/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 from django.http.request import HttpRequest
 from .services.DeezerApiService import DeezerApiService
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("Welcome to musicfeed")
 
+
 def getArtistRecords(request, artist_id: int):
     service = DeezerApiService()
-    artist_data = service.find_artist_data(artist_id)
-    if not artist_data:
+    artist = service.find_artist_data(artist_id)
+    if not artist:
         return HttpResponse("Artist not found.")
-    return HttpResponse(artist_data.name)
+    return HttpResponse(f"Name : {artist.name}" + "\nAlbums : \n" 
+                        + "\n".join([f"    {record.title} - {record.release_date}" for record in artist.records]))
